2b7cc8b20195d0ea070934bc9a7c0ee45ae5d38c
StarrFlower <starrflower@proton.me>
2026-05-26 15:18:54 -0700

    datagen

--- src/main/java/net/starrch/mossymayhem/MossyMayhemDataGenerator.java
+++ src/main/java/net/starrch/mossymayhem/MossyMayhemDataGenerator.java
@@ -2,10 +2,13 @@ package net.starrch.mossymayhem;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.starrch.mossymayhem.datagen.ModModelProvider;
 
 public class MossyMayhemDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
---
+++ src/main/java/net/starrch/mossymayhem/datagen/ModModelProvider.java
@@ -0,0 +1,24 @@
+package net.starrch.mossymayhem.datagen;
+
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.starrch.mossymayhem.item.ModItems;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricPackOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+        itemModelGenerators.generateFlatItem(ModItems.MOSS_CLUMP, ModelTemplates.FLAT_ITEM);
+    }
+}
