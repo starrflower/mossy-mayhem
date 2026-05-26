519ff1448702907eef8035b49f66e1f0af0ae72f
StarrFlower <starrflower@proton.me>
2026-05-26 15:09:43 -0700

    added item function

--- src/main/java/net/starrch/mossymayhem/MossyMayhem.java
+++ src/main/java/net/starrch/mossymayhem/MossyMayhem.java
@@ -2,6 +2,7 @@ package net.starrch.mossymayhem;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.starrch.mossymayhem.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +16,7 @@ public class MossyMayhem implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		MossyMayhem.LOGGER.info("Initializing " + MossyMayhem.MOD_ID);
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/starrch/mossymayhem/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.starrch.mossymayhem.item;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.starrch.mossymayhem.MossyMayhem;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item MOSS_CLUMP = registerItem("moss_clump", Item::new);
+
+
+
+
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(MossyMayhem.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MossyMayhem.MOD_ID, name)))));
+    }
+
+
+
+    public static void registerModItems() {
+        MossyMayhem.LOGGER.info("Registering Mod Items for " + MossyMayhem.MOD_ID);
+
+
+    }
+}
