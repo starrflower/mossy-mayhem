815740e418e535da9d7e979c10ab381d5ab57af1
StarrFlower <starrflower@proton.me>
2026-05-26 21:02:59 -0700

    added texture for dried moss block

--- src/main/java/net/starrch/mossymayhem/datagen/ModModelProvider.java
+++ src/main/java/net/starrch/mossymayhem/datagen/ModModelProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.model.ModelTemplates;
+import net.starrch.mossymayhem.block.ModBlocks;
 import net.starrch.mossymayhem.item.ModItems;
 
 public class ModModelProvider extends FabricModelProvider {
@@ -14,7 +15,7 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
-
+        blockModelGenerators.createTrivialCube(ModBlocks.DRIED_MOSS_BLOCK);
     }
 
     @Override
